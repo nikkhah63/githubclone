043471f2c84f1300a027c5e0f93162bfd7a1004a
z.nikkhah <nikkhah63@gmail.com>
2022-07-31 21:59:14 +0430

    گرافیک لیست

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -10,25 +10,43 @@ export default {
    //   },
    data() {
       return {
-         repository: {},
-         id: ""
+         item: {},
+         id: "",
+         isLoading: false
       };
    },
    beforeCreate: function () {
-    
-        axios
-      .get(`https://api.github.com/repositories/`+this.id)
+
+      axios
+         .get(`https://api.github.com/repositories/` + this.id)
          .then(result => {
-            this.repository = result.data;
+
+            this.item = result.data;
          })
    },
-   created: function () {
-      console.log('created!')
-    
-   },
-   beforeMount: function () {
+   beforeMount() {
+      axios.interceptors.request.use(
+         (config) => {
+            this.isLoading = true;
+            return config;
+         },
+         (error) => {
+            this.isLoading = false;
+            return Promise.reject(error);
+         }
+      );
 
-      console.log('Before Mount!')
+      axios.interceptors.response.use(
+         (response) => {
+            this.isLoading = false;
+            return response;
+         },
+         (error) => {
+            this.isLoading = false;
+            return Promise.reject(error);
+         }
+      );
+
 
    },
 
@@ -38,9 +56,28 @@ export default {
 </script>
 <template>
    <div class="container">
+      <div class="spinner-border text-dark " role="status" v-if="isLoading">
+         <span class="sr-only">Loading...</span>
+      </div>
       <div class="col-md-12">
-         <p>{{ repository?.name }}</p>
-         
+         <div class="card ">
+
+
+            <div class="card-body d-flex flex-cloumn  align-items-center">
+               <img :src="`${item.owner?.avatar_url}`" class="img-fluid img-thumbnail rounded-circle w-10 me-2" />
+               <div>
+                  <h5 class="card-title text-muted">{{ item.name }} </h5>
+                  <p class="card-title bold">{{ item.full_name }}</p>
+                  <p class="card-text text-muted text-justify ">{{ item.description }}</p>
+               </div>
+
+            </div>
+            <div class="card-body text-end">
+               <a href="#" class="btn btn-primary btn-sm card-link align-self-start right">download</a>
+            </div>
+         </div>
+
+
       </div>
    </div>
 </template>
--- src/components/List.vue
+++ src/components/List.vue
@@ -48,12 +48,10 @@ export default {
    beforeMount() {
       axios.interceptors.request.use(
          (config) => {
-            // alert("yes");
             this.isLoading = true;
             return config;
          },
          (error) => {
-            //  alert("no");
             this.isLoading = false;
             return Promise.reject(error);
          }
@@ -62,12 +60,10 @@ export default {
       axios.interceptors.response.use(
          (response) => {
             this.isLoading = false;
-            //alert("no");
             return response;
          },
          (error) => {
             this.isLoading = false;
-            //  alert("no");
             return Promise.reject(error);
          }
       );
@@ -87,15 +83,34 @@ export default {
 
 
    <div class="container">
-   
-     
+
+
       <!-- </div> -->
-      <div class="col-md-12" v-for="(item, key) in repository" :key="key">
+      <div class="row g-3">
+         <div class="col-md-12 " v-for="(item, key) in repository" :key="key">
 
-         <router-link :to="`/${item.id}`">{{ item.id }} : {{ item.full_name }}</router-link>
+            <router-link :to="`/${item.id}`" class="text-decoration-none">
+               <div class="card ">
+                
 
+                  <div class="card-body d-flex flex-cloumn  align-items-center">
+                      <img  :src="`${item.owner?.avatar_url}`"   class="img-fluid img-thumbnail rounded-circle w-10 me-2" />
+                     <div>
+                        <h5 class="card-title text-muted">{{ item.name }} </h5>
+                        <p  class="card-title bold">{{ item.full_name }}</p>                       
+                        <p class="card-text text-muted text-justify ">{{ item.description }}</p>
+                     </div>
+                     
+                  </div>
+                  <div class="card-body text-end" >
+                     <a href="#" class="btn btn-primary btn-sm card-link align-self-start right">Details</a>
+                  </div>
+               </div>
+            </router-link>
+
+         </div>
       </div>
-       <div class="spinner-border text-dark " role="status" v-if="isLoading">
+      <div class="spinner-border text-dark " role="status" v-if="isLoading">
          <span class="sr-only">Loading...</span>
       </div>
    </div>
